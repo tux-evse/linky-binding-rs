c18dc439d58bc986a2dca1c42a14eed84d96d317
Fulup Ar Foll <fulup@iot.bzh>
2025-12-21 23:06:35 +0000

    optional addon of event/verb name to mqtt topic

--- afb-binding/etc/binding-serial.sh
+++ afb-binding/etc/binding-serial.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd `dirname $0`
+export BASEDIR=$( readlink -e $( dirname $0 )/..)
 
 # use libafb development version if any
 export LD_LIBRARY_PATH="/usr/local/lib64:$LD_LIBRARY_PATH"
-export PATH="/usr/local/lib64:$PATH"
+export LD_LIBRARY_PATH=$BASEDIR/../target/debug
 clear
 
 echo "debug with: tio /dev/ttyUSB0"
--- afb-binding/etc/binding-udp.sh
+++ afb-binding/etc/binding-udp.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-cd `dirname $0/../..`
+export BASEDIR=$( readlink -e $( dirname $0 )/..)
 
 # use libafb development version if any
-export LD_LIBRARY_PATH="/usr/local/lib64:$LD_LIBRARY_PATH"
-export PATH="/usr/local/lib64:$PATH"
-clear
+export LD_LIBRARY_PATH="/usr/redpesk/linky-binding-rs/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH=$BASEDIR/../target/debug
 
+clear
 echo "debug with: socat - UDP4-LISTEN:2000"
+pkill afb-linky
+
 
 # start binder with test config
-afb-binder -vvv --config=afb-binding/etc/binding-linky-network.yaml --port=1236
+afb-binder -vvv --config=$BASEDIR/etc/binding-linky-network.yaml --port=1236
